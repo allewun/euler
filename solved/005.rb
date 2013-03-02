f6a6a4287a51b4087663948dec3f88575c55f556
Allen Wu <allewun@yahoo.com>
2013-03-01 21:49:27 -0800

    euler005 rewrite

--- solved/005.rb
+++ solved/005.rb
@@ -1,29 +1,28 @@
 #============================================================================
 # euler005
-# 6/20/12
+# 6/20/12, 3/1/13 rewritten
 #
 # What is the smallest positive number that is
 # evenly divisible by all of the numbers from 1 to 20?
 #============================================================================
 
 def euler005
-  nums = (11..20)
-  target = 20
+  num = 2
 
-  # how to do this part in ruby?
-  while true
-    good = true
-    nums.each {
-      |n|
-      if target % n != 0
-        good = false
-        break
-      end
-    }
-    if good
-      return target
+  loop do
+    if num.divisible?(19) &&
+       num.divisible?(17) &&
+       num.divisible?(13) &&
+       num.divisible?(11) &&
+       num.divisible?(12) &&
+       num.divisible?(14) &&
+       num.divisible?(15) &&
+       num.divisible?(16) &&
+       num.divisible?(18) &&
+       num.divisible?(20)
+      return num
     else
-      target += 1
+      num += 2 # only consider even numbers
     end
   end
 end
